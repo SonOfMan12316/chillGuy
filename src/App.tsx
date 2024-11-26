9bdeb004e843fd527020dba3a13768323b2cd9f9
SonOfMan12316 <mancycarter@gmail.com>
2024-11-26 07:23:45 +0100

    setup tailwind along w current day and time functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import react, { useState } from "react";
+import useFormattedDayAndTime from "./lib/utils/date";
+import ChillGuySection from "./components/drag-section";
 
 const App = () => {
-  return <div></div>;
+  const time = useFormattedDayAndTime();
+  return (
+    <div className="mx-4 py-4">
+      <div className="font-iBM text-xs">{time}</div>
+      <ChillGuySection />
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     extend: {},
     fontFamily: {
       poppins: ['Poppins', 'ui-sans-serif'],
+      iBM: ['IBM Plex Mono', 'monospace']
     },
+    colors: {
+      'ch-chill-guy': '#8F9A79',
+    }
   },
   plugins: [],
   // mode: 'jit',
