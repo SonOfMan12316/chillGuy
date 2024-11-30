3dd82910808010f5f07e5c7e782982b52e560c2a
SonOfMan12316 <mancycarter@gmail.com>
2024-11-30 13:50:26 +0100

    currentTime styling updated

---
+++ src/lib/utils/date.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const useFormattedDayAndTime = (): string => {
+  const [formattedDate, setFormattedDate] = useState<string>("");
+
+  useEffect(() => {
+    const updateDayAndTime = () => {
+      const dayAbbreviations = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thur",
+        "Fri",
+        "Sat",
+      ];
+      const date = new Date();
+      const hour = date.getHours();
+      const minute = date.getMinutes().toString().padStart(2, "0");
+      const dayAbbreviation = dayAbbreviations[date.getDay()];
+      const ampm = hour >= 12 ? "PM" : "AM";
+
+      setFormattedDate(`${dayAbbreviation} ${hour}:${minute} ${ampm}`);
+    };
+
+    updateDayAndTime();
+    const interval = setInterval(updateDayAndTime, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="currentTime py-8 px-6 uppercase">
+      {formattedDate}
+    </div>
+  );
+};
+
+export default useFormattedDayAndTime;
