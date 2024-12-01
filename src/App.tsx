439fa89036f01935ced067311200d34ed79fe4d5
SonOfMan12316 <mancycarter@gmail.com>
2024-12-01 21:58:56 +0100

    kickstarted option section bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import useFormattedDayAndTime from "./lib/utils/date";
 import ChillGuySection from "./components/drag-section";
+import Option from "./components/option";
 
 const App = () => {
   const time = useFormattedDayAndTime();
   return (
-    <div className="mx-2">
+    <div className="bg bg-opacity-100 h-full w-screen app">
       <div className="font-iBM text-xs">{time}</div>
-      <ChillGuySection />
+      <div className="overflow-scroll">
+        <div className="flex flex-col gap-8">
+          <ChillGuySection />
+          <Option />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/option.tsx
@@ -0,0 +1,11 @@
+const Option = () => {
+  return (
+    <div className="w-full sm:w-11/12 sm:mx-auto h-fit rounded-lg p-6 bg-white border-dark-grey border">
+      <div className="background-bit bg-white">
+        <h1>Background</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Option;
