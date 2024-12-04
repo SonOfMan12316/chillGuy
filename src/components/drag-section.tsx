e086a19453316adfc3834425ac1902b5f118aa70
SonOfMan12316 <mancycarter@gmail.com>
2024-12-04 16:45:48 +0100

    styling for primaryBgColor secondaryBgColor and backgroundImage also fixed the touchevent for smaller screens

--- src/components/drag-section.tsx
+++ src/components/drag-section.tsx
@@ -1,15 +1,46 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
 import chillGuy from "/images/chill-guy.png";
 
 type Position = {
   top: Number;
   right: Number;
+};
+
+interface DragSectionProp {
+  primaryBgColor: string;
+  secondaryBgColor: string | null;
+  file: File | null;
+  clickedButton: number | null;
 }
 
-const DragSection: React.FC = () => {
+const DragSection: React.FC<DragSectionProp> = ({
+  primaryBgColor,
+  secondaryBgColor,
+  file,
+  clickedButton,
+}) => {
   const dragableRef = useRef<HTMLImageElement | null>(null);
   const dragzoneRef = useRef<HTMLDivElement | null>(null);
   const [position, setPosition] = useState<Position>({ top: 140, right: 140 });
+
+  const backgroundStyle = useMemo(() => {
+    if (file) {
+      return {
+        backgroundImage: `url(${URL.createObjectURL(file)})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundColor: "",
+      };
+    } else if (clickedButton === 2 && primaryBgColor && !file) {
+      return { backgroundColor: primaryBgColor };
+    } else if (primaryBgColor && !secondaryBgColor) {
+      return { backgroundColor: primaryBgColor };
+    } else if (primaryBgColor && secondaryBgColor) {
+      return {
+        backgroundImage: `linear-gradient(to right, ${primaryBgColor}, ${secondaryBgColor})`,
+      };
+    }
+  }, [primaryBgColor, secondaryBgColor, file]);
 
   useEffect(() => {
     const dragElement = (element: HTMLElement, dragzone: HTMLElement) => {
@@ -22,6 +53,8 @@ const DragSection: React.FC = () => {
         document.onmouseup = null;
         document.onmousemove = null;
         document.ontouchend = null;
+        document.ontouchmove = null;
+
         element.classList.remove("drag");
       };
 
@@ -57,39 +90,61 @@ const DragSection: React.FC = () => {
         pos4 = clientY;
         element.classList.add("drag");
 
-        document.onmouseup = dragMouseUp;
         document.onmousemove = dragMouseMove;
-        document.addEventListener('touchstart', dragMouseDown, { passive: false });
+        document.onmouseup = dragMouseUp;
         document.ontouchmove = dragMouseMove;
         document.ontouchend = dragMouseUp;
       };
 
-      dragzone.onmousedown = dragMouseDown;
-      dragzone.ontouchstart = dragMouseDown;
+      dragzone.onmousedown = (event) => {
+        if (event.target === element) dragMouseDown(event);
+      };
+
+      dragzone.ontouchstart = (event) => {
+        if (event.target === element) dragMouseDown(event);
+      };
     };
 
     if (dragableRef.current && dragzoneRef.current) {
       dragElement(dragableRef.current, dragzoneRef.current);
     }
-    setPosition({top: 140, right: 140 })
+    setPosition({ top: 140, right: 140 });
+
+    return () => {
+      if (dragzoneRef.current) {
+        dragzoneRef.current.onmousedown = null;
+        dragzoneRef.current.ontouchstart = null;
+      }
+      document.onmousemove = null;
+      document.onmouseup = null;
+      document.ontouchmove = null;
+      document.ontouchend = null;
+    };
   }, []);
 
   return (
-    <div
-      ref={dragzoneRef}
-      className="overflow-hidden relative mt-20 rounded-md bg-ch-chill-guy h-[350px] w-full"
-    >
-      <div className="">
-        <img
-          ref={dragableRef}
-          src={chillGuy}
-          alt="Draggable"
-          className="cursor-grab absolute h-1/2 w-auto object-contain"
-          style={{
-            top: `${position.top}px`,
-            right: `${position.right}px`
-          }}
-        />
+    <div className="w-full md:flex-1 md:ml-6 flex justify-center overflow-scroll">
+      <div className="w-full md:flex md:flex-1 md:ml-6">
+        <div className="relative bg-transparent mx-auto h-[350px] w-[350px] lg:h-[600px] lg:w-[600px]">
+          <div
+            ref={dragzoneRef}
+            style={{
+              ...backgroundStyle,
+            }}
+            className="relative rounded-xl h-full w-full shadow-lg overflow-hidden"
+          >
+            <img
+              ref={dragableRef}
+              src={chillGuy}
+              alt="Draggable"
+              className="cursor-grab absolute w-auto h-1/2 object-contain"
+              style={{
+                top: `${position.top}px`,
+                right: `${position.right}px`,
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
