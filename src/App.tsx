b17896627ee53b4dcd853f349850d1a9c6db7db1
SonOfMan12316 <mancycarter@gmail.com>
2024-12-04 16:44:55 +0100

    added option section where users style their chill guy to their taste

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import React, { useState } from "react";
 import useFormattedDayAndTime from "./lib/utils/date";
-import ChillGuySection from "./components/drag-section";
+import DragSection from "./components/drag-section";
 import Option from "./components/option";
 
 const App = () => {
   const time = useFormattedDayAndTime();
+  const [primaryBgColor, setPrimaryBgColor] = useState<string>("#295144");
+  const [secondaryBgColor, setSecondaryBgColor] = useState<string>("#A25353");
+  const [file, setFile] = useState<File | null>(null);
+  const [clickedButton, setClickedButton] = useState<number | null>(0);
+
   return (
-    <div className="bg bg-opacity-100 h-full w-screen app">
-      <div className="font-iBM text-xs">{time}</div>
-      <div className="overflow-scroll">
-        <div className="flex flex-col gap-8">
-          <ChillGuySection />
-          <Option />
+    <main className="bg-gray-100 overflow-y-auto h-screen">
+      <header className="font-iBM text-xs">{time}</header>
+      <div className="mt-20 2xl:mt-40 flex items-center justify-center overflow-y-auto">
+        <div className="flex w-screen flex-col-reverse md:flex-row items-start gap-8 md:gap-0 md:items-center md:max-w-[1056px] mx-auto h-full">
+          <Option
+            primaryBgColor={primaryBgColor}
+            secondaryBgColor={secondaryBgColor}
+            clickedButton={clickedButton}
+            setPrimaryBgColor={setPrimaryBgColor}
+            setSecondaryBgColor={setSecondaryBgColor}
+            setFile={setFile}
+            setClickedButton={setClickedButton}
+          />
+          <DragSection
+            primaryBgColor={primaryBgColor}
+            secondaryBgColor={secondaryBgColor}
+            file={file}
+            clickedButton={clickedButton}
+          />
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
--- src/components/option.tsx
+++ src/components/option.tsx
@@ -1,8 +1,119 @@
-const Option = () => {
+import React, { useState } from "react";
+
+interface OptionsProps {
+  primaryBgColor: string;
+  secondaryBgColor: string;
+  clickedButton: number | null;
+  setPrimaryBgColor: React.Dispatch<React.SetStateAction<string>>;
+  setSecondaryBgColor: React.Dispatch<React.SetStateAction<string>>;
+  setFile: React.Dispatch<React.SetStateAction<File | null>>;
+  setClickedButton: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+const Option: React.FC<OptionsProps> = ({
+  primaryBgColor,
+  secondaryBgColor,
+  clickedButton,
+  setPrimaryBgColor,
+  setSecondaryBgColor,
+  setFile,
+  setClickedButton,
+}) => {
+  const styles: string[] = ["solid", "gradient", "image"];
+
+  const handleClick = (index: number) => {
+    setClickedButton(index === clickedButton ? null : index);
+
+    if (index === 0) {
+      setPrimaryBgColor((prev) => prev || "#295144");
+      setSecondaryBgColor("");
+      setFile(null);
+    } else if (index === 1) {
+      setSecondaryBgColor((prev) => prev || "#A25353");
+      setFile(null);
+    } else if (index === 2) {
+      setSecondaryBgColor("");
+    }
+  };
+
+  const handlePrimaryColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPrimaryBgColor(e.target.value);
+  };
+
+  const handleSecondaryColorChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    setSecondaryBgColor(e.target.value);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] || null;
+    setFile(selectedFile);
+  };
+
   return (
-    <div className="w-full sm:w-11/12 sm:mx-auto h-fit rounded-lg p-6 bg-white border-dark-grey border">
+    <div className="w-screen md:w-80 md:min-w-[320px] h-fit rounded-lg p-6 bg-white border">
       <div className="background-bit bg-white">
-        <h1>Background</h1>
+        <h1 className="text-base mb-5">Background</h1>
+        <div className="grid grid-cols-3 gap-2 mb-4">
+          {styles.map((style, index) => (
+            <button
+              key={index}
+              onClick={() => handleClick(index)}
+              className={`text-xs h-8 flex items-center justify-center rounded-[6px] border capitalize ${
+                clickedButton === index ? "text-black" : "opacity-25"
+              }`}
+            >
+              {style}
+            </button>
+          ))}
+        </div>
+        <div className="styling section flex flex-col">
+          {clickedButton === 2 ? (
+            <>
+              <label
+                htmlFor="file-upload"
+                className="w-full p-6 border-[2px] border-dashed border-gray-200 rounded-lg hover:bg-gray-50 flex flex-col items-center gap-2"
+              >
+                <span>Upload Background</span>
+              </label>
+              <input
+                id="file-upload"
+                accept="image/*"
+                className="hidden"
+                type="file"
+                onChange={handleFileChange}
+              />
+            </>
+          ) : (
+            <>
+              <label className="text-sm my-2 text-gray-600">
+                Primary Color
+              </label>
+              <input
+                className="h-10 w-10 bg-white pointer-events-auto"
+                type="color"
+                onChange={handlePrimaryColorChange}
+                value={primaryBgColor}
+              />
+              {clickedButton === 1 && (
+                <>
+                  <label className="text-sm my-2 text-gray-600">
+                    Secondary Color
+                  </label>
+                  <input
+                    className="h-10 w-10 bg-white pointer-events-auto"
+                    type="color"
+                    onChange={handleSecondaryColorChange}
+                    value={secondaryBgColor}
+                  />
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
